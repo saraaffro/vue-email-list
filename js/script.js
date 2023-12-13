146a55d286db01c870b9e301db6349bf86475cb4
saraaffro <saraaffro@gmail.com>
2023-12-13 13:09:39 +0100

    feat: create mail array with api random email

---
+++ js/script.js
@@ -0,0 +1,86 @@
+/*
+Attraverso l’apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+*/
+
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return {
+            mailArray: [],
+            mail: "",
+        }
+    },
+    mounted(){
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((email) => {
+            this.email = email.data.response;
+            this.mailArray.push(this.email);
+            console.log(this.email);
+            console.log(this.mailArray);
+        })
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((email) => {
+            this.email = email.data.response;
+            this.mailArray.push(this.email);
+            console.log(this.email);
+            console.log(this.mailArray);
+        })
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((email) => {
+            this.email = email.data.response;
+            this.mailArray.push(this.email);
+            console.log(this.email);
+            console.log(this.mailArray);
+        })
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((email) => {
+            this.email = email.data.response;
+            this.mailArray.push(this.email);
+            console.log(this.email);
+            console.log(this.mailArray);
+        })
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((email) => {
+            this.email = email.data.response;
+            this.mailArray.push(this.email);
+            console.log(this.email);
+            console.log(this.mailArray);
+        })
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((email) => {
+            this.email = email.data.response;
+            this.mailArray.push(this.email);
+            console.log(this.email);
+            console.log(this.mailArray);
+        })
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((email) => {
+            this.email = email.data.response;
+            this.mailArray.push(this.email);
+            console.log(this.email);
+            console.log(this.mailArray);
+        })
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((email) => {
+            this.email = email.data.response;
+            this.mailArray.push(this.email);
+            console.log(this.email);
+            console.log(this.mailArray);
+        })
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((email) => {
+            this.email = email.data.response;
+            this.mailArray.push(this.email);
+            console.log(this.email);
+            console.log(this.mailArray);
+        })
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((email) => {
+            this.email = email.data.response;
+            this.mailArray.push(this.email);
+            console.log(this.email);
+            console.log(this.mailArray);
+        })
+    }
+}).mount("#app")
